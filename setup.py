8ef89db345c05e3d706e9e0cd5582f79c72d9f83
Mikhail Lukashevich <mikhail.n.lukashevich@gmail.com>
2024-08-15 13:32:13 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Mikhail Lukashevich',
+    packages= find_packages(),
+    install_requires = []
+
+)
